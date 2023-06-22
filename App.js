2b642cb804b07922808fdaa2039571b8c5e952aa
Akshat Nehra <akshatnehra1999@gmail.com>
2023-06-22 15:07:19 -0400

    chat-service: Bottom Navbar, Chat Support FAB and signout in Profile

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import { FAB } from 'react-native-paper';
+import React, { useEffect, useState } from "react";
+import { FAB } from "react-native-paper";
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+import Home from "./screens/Home/Home";
+import Profile from "./screens/Profile/Profile";
+import MyLeaning from "./screens/MyLearning/MyLearning";
+
+const Tab = createMaterialBottomTabNavigator();
 
 import {
   StyleSheet,
@@ -8,24 +18,43 @@ import {
   TextInput,
   Pressable,
   SafeAreaView,
-} from 'react-native';
-import {API, graphqlOperation} from 'aws-amplify';
-import {createTodo} from './src/graphql/mutations';
-import {listTodos} from './src/graphql/queries';
+} from "react-native";
+// import {API, graphqlOperation} from 'aws-amplify';
+// import {createTodo} from './src/graphql/mutations';
+// import {listTodos} from './src/graphql/queries';
 import {
   withAuthenticator,
   useAuthenticator,
-} from '@aws-amplify/ui-react-native';
+} from "@aws-amplify/ui-react-native";
 
-import { Amplify } from 'aws-amplify';
-import awsExports from './src/aws-exports';
+import { Amplify } from "aws-amplify";
+import awsExports from "./src/aws-exports";
 Amplify.configure(awsExports);
 
 // retrieves only the current value of 'user' from 'useAuthenticator'
-const userSelector = (context) => [context.user]
+const userSelector = (context) => [context.user];
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      {/* Add your content here */}
+    </View>
+  );
+};
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>Settings Screen</Text>
+      {/* Add your content here */}
+    </View>
+  );
+};
 
 const SignOutButton = () => {
   const { user, signOut } = useAuthenticator(userSelector);
+
   return (
     <Pressable onPress={signOut} style={styles.buttonContainer}>
       <Text style={styles.buttonText}>
@@ -35,76 +64,47 @@ const SignOutButton = () => {
   );
 };
 
-const initialFormState = {name: '', description: ''};
-
 const App = () => {
-  const [formState, setFormState] = useState(initialFormState);
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  function setInput(key, value) {
-    setFormState({...formState, [key]: value});
-  }
-
-  async function fetchTodos() {
-    try {
-      const todoData = await API.graphql(graphqlOperation(listTodos));
-      const todos = todoData.data.listTodos.items;
-      setTodos(todos);
-    } catch (err) {
-      console.log('error fetching todos');
-    }
-  }
-
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.description) return;
-      const todo = {...formState};
-      setTodos([...todos, todo]);
-      setFormState(initialFormState);
-      await API.graphql(graphqlOperation(createTodo, {input: todo}));
-    } catch (err) {
-      console.log('error creating todo:', err);
-    }
-  }
-
   return (
     <SafeAreaView style={styles.container}>
-      {/* <View style={styles.container}>
-        <SignOutButton />
-        <TextInput
-          onChangeText={value => setInput('name', value)}
-          style={styles.input}
-          value={formState.name}
-          placeholder="Name"
-        />
-        <TextInput
-          onChangeText={value => setInput('description', value)}
-          style={styles.input}
-          value={formState.description}
-          placeholder="Description"
-        />
-        <Pressable onPress={addTodo} style={styles.buttonContainer}>
-          <Text style={styles.buttonText}>Create todo</Text>
-        </Pressable>
-        {todos.map((todo, index) => (
-          <View key={todo.id ? todo.id : index} style={styles.todo}>
-            <Text style={styles.todoName}>{todo.name}</Text>
-            <Text style={styles.todoDescription}>{todo.description}</Text>
-          </View>
-        ))}
-      </View> */}
-
-
-
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Tab.Navigator>
+            <Tab.Screen
+              name="My Learning"
+              component={MyLeaning}
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <Icon name="book" color={color} size={24} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Home"
+              component={Home}
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <Icon name="home" color={color} size={24} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Profile"
+              component={() => <Profile SignOut={SignOutButton} />}
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <Icon name="account" color={color} size={24} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
 
       <FAB
         icon="forum"
         style={styles.fab}
-        onPress={() => console.log('Pressed')}
+        onPress={() => console.log("Pressed")}
       />
     </SafeAreaView>
   );
@@ -113,20 +113,25 @@ const App = () => {
 export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
-  container: {width: 400, flex: 1, padding: 20, alignSelf: 'center'},
-  todo: {marginBottom: 15},
-  input: {backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18},
-  todoName: {fontSize: 20, fontWeight: 'bold'},
+  container: { width: 400, flex: 1, padding: 20, alignSelf: "center" },
+  todo: { marginBottom: 15 },
+  input: {
+    backgroundColor: "#ddd",
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: "bold" },
   buttonContainer: {
-    alignSelf: 'center',
-    backgroundColor: 'black',
+    alignSelf: "center",
+    backgroundColor: "black",
     paddingHorizontal: 8,
   },
-  buttonText: {color: 'white', padding: 16, fontSize: 18},
+  buttonText: { color: "white", padding: 16, fontSize: 18 },
   fab: {
-    position: 'absolute',
+    position: "absolute",
     margin: 16,
     right: 16,
-    bottom: 0,
+    bottom: 100,
   },
 });
---
+++ screens/MyLearning/MyLearning.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyLearning = () => {
+  return (
+    <View>
+      <Text>MyLearning</Text>
+    </View>
+  )
+}
+
+export default MyLearning
