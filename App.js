63d4876d1a034e5163c65b0c17c717fb06c7bca8
anurag <anurag.sharma.2p@gmail.com>
2023-06-23 19:56:12 -0400

    backend/infrastructure: updated readme with backend infra configuration details

--- App.js
+++ App.js
@@ -120,29 +120,7 @@ const App = () => {
   );
 };
 
-export default withAuthenticator(App);
 
-const styles = StyleSheet.create({
-  container: { width: 400, flex: 1, padding: 20, alignSelf: "center" },
-  todo: { marginBottom: 15 },
-  input: {
-    backgroundColor: "#ddd",
-    marginBottom: 10,
-    padding: 8,
-    fontSize: 18,
-  },
-  todoName: { fontSize: 20, fontWeight: "bold" },
-  buttonContainer: {
-    alignSelf: "center",
-    backgroundColor: "black",
-    paddingHorizontal: 8,
-  },
-  buttonText: { color: "white", padding: 16, fontSize: 18 },
-  fab: {
-    position: "absolute",
-    margin: 16,
-    right: 16,
-    bottom: 100,
-  },
-});
+
 }
+export default withAuthenticator(App);
