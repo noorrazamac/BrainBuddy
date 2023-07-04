480bb54ffcc810ea9cec1306d8ca8769a014fcf3
Garima Jyoti <jyotigarima2000@gmail.com>
2023-07-04 10:32:07 -0400

    course-details: Added few courses, image of the course and styling

--- screens/CourseDetails/CourseDetails.js
+++ screens/CourseDetails/CourseDetails.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 
-const course = {
+const course = [
+{
   title: 'React Native 101',
   description: 'Learn the basics of React Native development',
   additionalDescription:
     'This course covers topics such as UI development, state management, navigation, and more. By the end of the course, you will have the skills to build mobile apps with React Native.',
   instructors: ['John Doe', 'Jane Smith', 'Mike Johnson'],
   duration: '4 weeks',
-};
+},
+{
+  title: 'Advanced React Native',
+  description: 'Take your React Native skills to the next level',
+  additionalDescription:
+    'This course dives deep into advanced topics in React Native development, including performance optimization, native module integration, and advanced UI techniques. By the end of the course, you will be equipped to build complex and high-performance mobile apps with React Native.',
+  instructors: ['Sarah Johnson', 'Michael Smith', 'Emily Davis'],
+  duration: '6 weeks',
+},
+{
+  title: 'React Native for Redux Enthusiasts',
+  description: 'Learn how to leverage Redux in React Native apps',
+  additionalDescription:
+    'This course is designed for developers familiar with Redux who want to integrate it with React Native. You will learn how to manage complex application state and data flow effectively.',
+  instructors: ['Alex Brown', 'Jessica Lee'],
+  duration: '5 weeks',
+},
+];
 
 const CourseDetails = () => {
   const screenWidth = Dimensions.get('window').width;
@@ -23,7 +41,12 @@ const CourseDetails = () => {
           <View style={styles.separator} />
         </View>
 
-      {/* Add image here */}
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('./assets/reactCourse.jpg')}
+            style={{ ...styles.image, width: imageWidth, height: imageHeight }}
+          />
+        </View>
 
         <View style={styles.detailsContainer}>
           <Text style={styles.label}>Description:</Text>
@@ -67,7 +90,13 @@ const styles = StyleSheet.create({
     marginTop: 8,
     marginBottom: 16,
   },
-  //Add Image styling
+  imageContainer: {
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  image: {
+    resizeMode: 'cover',
+  },
   detailsContainer: {
     marginBottom: 16,
   },
