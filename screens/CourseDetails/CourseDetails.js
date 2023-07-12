f69b1aca346f1b8e845bb8f135abbde6388208fe
Garima Jyoti <jyotigarima2000@gmail.com>
2023-07-12 10:23:43 -0400

    Course-Details: code fixes and style changes

--- screens/CourseDetails/CourseDetails.js
+++ screens/CourseDetails/CourseDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 
-const course = [
+const course = 
 {
   title: 'React Native 101',
   description: 'Learn the basics of React Native development',
@@ -9,24 +9,7 @@ const course = [
     'This course covers topics such as UI development, state management, navigation, and more. By the end of the course, you will have the skills to build mobile apps with React Native.',
   instructors: ['John Doe', 'Jane Smith', 'Mike Johnson'],
   duration: '4 weeks',
-},
-{
-  title: 'Advanced React Native',
-  description: 'Take your React Native skills to the next level',
-  additionalDescription:
-    'This course dives deep into advanced topics in React Native development, including performance optimization, native module integration, and advanced UI techniques. By the end of the course, you will be equipped to build complex and high-performance mobile apps with React Native.',
-  instructors: ['Sarah Johnson', 'Michael Smith', 'Emily Davis'],
-  duration: '6 weeks',
-},
-{
-  title: 'React Native for Redux Enthusiasts',
-  description: 'Learn how to leverage Redux in React Native apps',
-  additionalDescription:
-    'This course is designed for developers familiar with Redux who want to integrate it with React Native. You will learn how to manage complex application state and data flow effectively.',
-  instructors: ['Alex Brown', 'Jessica Lee'],
-  duration: '5 weeks',
-},
-];
+};
 
 const CourseDetails = () => {
   const screenWidth = Dimensions.get('window').width;
@@ -112,17 +95,17 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   label: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 8,
   },
   additionalDescription: {
-    fontSize: 18,
-    marginBottom: 10,
+    fontSize: 17,
+    marginBottom: 8,
   },
   description: {
-    fontSize: 18,
-    marginBottom: 10,
+    fontSize: 17,
+    marginBottom: 8,
   },
   instructorContainer: {
     flexDirection: 'row',
@@ -140,10 +123,10 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   instructor: {
-    fontSize: 16,
+    fontSize: 17,
   },
   duration: {
-    fontSize: 16,
+    fontSize: 17,
   },
 });
 
